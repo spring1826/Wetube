ba7d1bff668833e981bbed0c176eab2a8a6c6312
spring1826 <dongwon1826@gmail.com>
2020-05-12 23:58:48 +0900

    routes개념

---
+++ routers/userRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const userRouter = express.Router(); //router는 작은 단위의 행동들을 나눠서 만들고 제어할수 있는거?
+
+export default userRouter;
+//MVC
+//Modul data
+//View 데이터가 어떻게 생겼는지
+//Controller 데이터를 찾는 함수
